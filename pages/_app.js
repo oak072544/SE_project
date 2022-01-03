1f9edefddaa65d391f9cf07f5eda79ed1b30c727
Muhammad Mannir Ahmad <manniru@gmail.com>
2022-01-03 07:38:19 +0100

    Initialised Source Files

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import '../src/App.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/scan.jsx
@@ -0,0 +1,52 @@
+// import '../src/App.css';
+
+import React from 'react';
+import Html5QrcodePlugin from '../src/Html5QrcodePlugin.jsx'
+import ResultContainerPlugin from '../src/ResultContainerPlugin.jsx'
+import HowToUse from '../src/HowToUse.jsx'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      decodedResults: []
+    }
+
+    // This binding is necessary to make `this` work in the callback.
+    this.onNewScanResult = this.onNewScanResult.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <section className="App-section">
+          <div className="App-section-title"> Html5-qrcode Next JS By Brilliant.ng</div>
+          <br />
+          <br />
+          <br />
+          <Html5QrcodePlugin 
+            fps={10}
+            qrbox={250}
+            disableFlip={false}
+            qrCodeSuccessCallback={this.onNewScanResult}/>
+          <ResultContainerPlugin results={this.state.decodedResults} />
+          <HowToUse />
+        </section>
+      </div>
+    );
+  }
+
+  onNewScanResult(decodedText, decodedResult) {
+    console.log(
+      "App [result]", decodedResult);
+
+    // let decodedResults = this.state.decodedResults;
+    // decodedResults.push(decodedResult);
+    this.setState((state, props) => {
+      state.decodedResults.push(decodedResult);
+      return state;
+    });
+  }
+}
+
+export default App;
